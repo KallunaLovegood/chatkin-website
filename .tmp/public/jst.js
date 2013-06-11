a55c427b6647100b585a52be6ee9aa7b9848a4f1
hdesignsit <heather@balderdash.co>
2013-06-11 12:53:01 -0500

    adding styles to home

---
+++ .tmp/public/jst.js
@@ -0,0 +1,21 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["assets/templates/App.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<region data-id="content" default="Home"></region>\n';
+
+}
+return __p
+};
+
+this["JST"]["assets/templates/Home.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="home-page">\n\n\n<!-- MAIN NAVIGATION -->\n\n\t<div class="main-nav">\n\t\t<div class="container">\n\t\t\t<div id="branding">\n\t\t\t\t<h1 id="logo">Chatkin</h1>\n\t\t\t</div>\n\n\t\t\t<ul>\n\t\t\t\t<li>Demo</li>\n\t\t\t\t<li>Pricing</li>\n\t\t\t\t<li>Partners</li>\n\t\t\t\t<li>Twitter</li>\n\t\t\t\t<li id="sign-up">Sign Up</li>\n\t\t\t\t<li id="log-in">Log In</li>\n\t\t\t</ul>\n\t\t</div>\n\t</div>\n\n\n\n<!-- (ABOVE THE FOLD SECTION) \n\tLIGHT BLUE HEADER -->\n\n\t<div id="light-blue-header">\n\t\t<div class="container">\n\t\t\t<div id="top-left-text">\n\t\t\t\t<h3>In the past</h3>\n\t\t\t\t<p>When you get that urge to share something onlline, you have to reroute to another social networking site like Twitter, or Facebook.</p>\n\t\t\t</div>\n\t\t\t<div id="chatkin-head"></div>\n\t\t\t<div id="spaceship-on-cloud"></div>\n\t\t\t<div id="social-balloons"></div>\n\t\t\t<div id="bot-right-text">\n\t\t\t\t<h3>Chatkin has landed</h3>\n\t\t\t\t<p>But why reroute to another page when you can say everything you want from the comfort of your current website? Chatkin makes the entire web seem a little less lonely, and a lot more personal! Chatkin wants to make every page a social network of its own!</p>\n\t\t\t</div>\n\t\t</div>\n\t</div>\n\n\n\n\n<!-- CALL TO ACTION SECTION \n\tFOR URL REQUEST -->\n\t<div class="container">\n\n\t\t<div id="call-to-action-input">\n\t\t\t<h2>Don\'t Have An Account?</h2>\n\t\t\t<input type="text" id="call-to-action-input" placeholder="What\'s Your Website\'s URL?" />\n\t\t\t<div id="sign-me-up-button">\n\t\t\t\t<p>Sign Me Up!</p>\n\t\t\t</div>\n\t\t</div>\n\t\t<div id="shadow-image"></div>\n\n\n\n<!-- SECTION FOR ALL THE "LEARN\n\tMORE" OPTIONS -->\n\n\t\t<div id="three-things">\n\t\t\t<h1>3 Things Chatkin Can Do For You</h1>\n\t\t\t<div class="three-things">\n\t\t\t\t<div id="grow">\n\t\t\t\t\t<h2>Grow Your Own Community</h2>\n\t\t\t\t\t<!-- We\'ll put the image as background of the div -->\n\t\t\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla vel ante quam. Etiam eu est in tortor</p>\n\t\t\t\t\t<div class="small-button">\n\t\t\t\t\t\t<p>Learn More...</p>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\n\t\t\t\t<div id="capture">\n\t\t\t\t\t<h2>Capture Sign-Ups</h2>\n\t\t\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla vel ante quam. Etiam eu est in tortor</p>\n\t\t\t\t\t<div class="small-button">\n\t\t\t\t\t\t<p>Learn More...</p>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\n\t\t\t\t<div id="no-installation">\n\t\t\t\t\t<h2>No Installation</h2>\n\t\t\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla vel ante quam. Etiam eu est in tortor</p>\n\t\t\t\t\t<div class="small-button">\n\t\t\t\t\t\t<p>Learn More...</p>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</div>\n\n\n\n\n\n\n</div>\n\n\n\n\n\n\n\n\n\n\n\n\n\n';
+
+}
+return __p
+};
